9eaa05f9318df568a37e1b01e7009d3d513004f8
mika <kaneda.1018@gmail.com>
2018-10-02 15:18:58 +0900

    10/02/18

--- test_scripts/test.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-n = int(input())
-w = [input() for i in range(n)]
-res = w[0]
-
-for i in range(1, n - 1):
-    for j in range(0, min(len(w[i], w[i + 1]))):
-
