021c4e75e4057230242ce1b21744181c8ba271c5
mkanenobu <kaneda.1018@gmail.com>
2018-11-17 23:55:13 +0900

    11/17/18

--- nim/httpGet.nim
+++ nim/httpGet.nim
@@ -1,3 +1,10 @@
-import httpclient
-var client = newHttpClient()
-echo client.getContent("https://mkanenobu.github.io/")
+import httpclient, os, strformat
+
+if paramCount() == 0:
+  quit 1
+
+var
+  url = paramStr(1)
+  client = newHttpClient()
+
+echo client.getContent(fmt"{url}")
--- pascal/if_state.pas
+++ pascal/if_state.pas
@@ -6,7 +6,9 @@ begin
   readln(n);
   if n mod 3 = 0 then
     writeln('Can divide by 3!!!')
+  else if n mod 4 = 0 then
+    writeln('Cant divide by 4!!!')
   else
-    writeln('Cant divide by 3');
+    writeln('Other');
 
 end.
