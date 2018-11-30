193018ed3a0332fdb92c48da5383638bbb8a30a7
Kanenobu Mitsuru <mkanenobu@users.noreply.github.com>
2018-11-30 16:57:32 +0900

    Create decimal.nim

---
+++ nim/decimal.nim
@@ -0,0 +1,36 @@
+import math, sequtils, strutils
+
+type
+  Decimal = tuple[integer: seq[int], decimal: seq[int]]
+  BCD = tuple[integer: seq[string], decimal: seq[string]]
+
+proc toBCD*(floatString: string): BCD =
+  proc toBCD*(floatString: string): BCD =
+  ## Float to BCD
+  ##
+  ##-
+  var
+    mpInt, tmpDec: string
+    resInt: seq[string] = @[]
+    resDec: seq[string] = @[]
+    (tmpInt, tmpDec) = floatString.split('.')
+    for i in tmpInt:
+      resInt.add(toBin(parseInt($i), 4))
+    for i in tmpDec:
+      resDec.add(toBin(parseInt($i), 4))
+    return (resInt, resDec)
+ 
+proc BcdToFloat*(d: BCD): float =
+  ## Decimal to float
+  ##
+  ## raise Overflow Error
+  var
+    tmpInt = d.integer
+    tmpDec = d.decimal
+    res: string = ""
+  for i in tmpInt:
+    res &= parseBinInt(i)
+  res &= '.'
+  for i in tmpDec:
+    res &= parseBinInt(i)
+  return parseFloat(res)
