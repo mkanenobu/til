512e2da973bc15206676a8cabc0b26d5a16a846f
mkanenobu <kaneda.1018@gmail.com>
2018-12-26 07:33:41 +0000

    delete documents

--- forth/forth_tutorial/js/docs.js
+++
@@ -1,7 +0,0 @@
-(window.onload = function() {
-  var elm = document.getElementsByTagName("h1")[0];
-  elm.insertAdjacentHTML('beforeend', document.title);
-
-  var header = document.getElementsByTagName("header")[0];
-  var filename = location.href.split('/').pop();
-});
