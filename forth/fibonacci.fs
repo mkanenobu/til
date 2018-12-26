3f0b8765e170ccbc4cf58ed89df0997b57a57fb9
mkanenobu <kaneda.1018@gmail.com>
2018-12-26 08:24:36 +0000

    12/26/18

--- forth/fibonacci.fs
+++ forth/fibonacci.fs
@@ -1,9 +1,9 @@
 \ print nth of fibonacci seq
 : fibo { n1 -- n2 }
-  n1 0= if 0 exit then
+  n1 0=  if 0 exit then
   n1 1 = if 1 exit then
   0 1
-  n1 1 - 0 DO
+  n1 1 DO
     2dup +
     rot drop
   LOOP
