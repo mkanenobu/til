7904930982a97fa1bd513934534c028bb045947c
mkanenobu <kaneda.1018@gmail.com>
2018-12-27 05:48:31 +0000

    12/27/18

--- forth/break_loop.fs
+++ forth/break_loop.fs
@@ -1,11 +1,21 @@
+: break_loop ( -- )
+  10 0 DO
+    i dup .
+    5 = IF LEAVE THEN
+  LOOP
+  ." last"
+;
 
-: break_in_loop ( -- )
+: exit_in_loop ( -- )
   10 0 DO
     i dup .
     2 * 10 = IF UNLOOP EXIT THEN
   LOOP
 ;
 
-break_in_loop
+break_loop
+CR
+
+exit_in_loop
 CR
 .s
