3ed1da707af406a40faa1280c5763cf0a8fa3f4f
mkanenobu <kaneda.1018@gmail.com>
2019-01-09 04:12:02 +0000

    01/09/19

--- forth/odd_even.fs
+++ forth/odd_even.fs
@@ -1,13 +1,13 @@
 
 : odd_or_even ( n -- )
-1 +
-0 DO
-i dup . 2 mod 0=
-IF ." is even."
-ELSE ." is odd."
-THEN
-CR
-LOOP
+  1 +
+  0 DO
+    i dup . 2 mod 0=
+      IF ." is even."
+      ELSE ." is odd."
+    THEN
+    CR
+  LOOP
 ;
 
 10
