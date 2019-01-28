17928bcc2686a48a43cd4000417b470d57010f29
mkanenobu <kaneda.1018@gmail.com>
2019-01-28 19:00:25 +0900

    01/28/19 07:00:25 PM

---
+++ others/haskell/filter.hs
@@ -0,0 +1,9 @@
+filter' :: (a->Bool) -> [a] -> [a]
+filter' _ [] = []
+filter' p (x:xs)
+  | p x       = x : filter' p xs
+  | otherwise = filter' p xs
+
+main = do
+  let l = [0..10]
+  print (filter' even l)
