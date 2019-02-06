bfb9173b77f4fe6bc70ee8aa1f8923050971001e
mkanenobu <kaneda.1018@gmail.com>
2019-02-06 16:39:18 +0900

    02/06/19 04:39:18 PM

--- haskell/dc.hs
+++ haskell/dc.hs
@@ -1,25 +1,26 @@
 {-
-Reverese Polish Notation calculator implementation in Haskell
+Reverese Polish calculator implementation in Haskell
 -}
 import System.IO
-import Data.IORef
 import Control.Monad
+import Control.Applicative
 
 type Stack = [Double]
 
+operaters :: [String]
 operaters = ["+", "-", "*", "/"]
 
 main :: IO ()
 main = do
-  forM_ [0..] $ \_ -> do
-    s <- readLine
-    -- forM_ (readIORef s) $ \i -> do
-    print $ sum $ map cast s
-  -- where
-  --   stack = push 4 (push 255 [1, 2, 3]::Stack)
+  line <- readLine
+  forM_ [0..(length line - 1)] $ \i -> do
+    let n = line !! i
+    let s = []
+    -- print $ strToDbl $ n
+    print $ calOrPush n s
 
-cast :: String -> Double
-cast n = read n::Double
+strToDbl :: String -> Double
+strToDbl n = read n::Double
 
 push :: Double -> Stack -> Stack
 push a xs = a : xs
@@ -28,17 +29,24 @@ pop :: Stack -> (Double, Stack)
 pop [] = error "Stack underflow"
 pop (x : xs) = (x, xs)
 
+calOrPush :: String -> Stack -> Stack
+calOrPush str s =
+  if str `elem` operaters
+     then calc str s
+     else push (strToDbl str) s
+
 readLine :: IO [String]
 readLine =
   words <$> getLine
 
-calc :: String -> Stack -> (Double, Stack)
+calc :: String -> Stack -> Stack
 calc w s =
   case w of
-    "+" -> (n1 + n2, s2)
-    "-" -> (n1 - n2, s2)
-    "*" -> (n1 - n2, s2)
-    "/" -> (n2 / n1, s2)
+    "+" -> (push (n1 + n2) s2)
+    "-" -> (push (n1 - n2) s2)
+    "*" -> (push (n1 - n2) s2)
+    "/" -> (push (n2 / n1) s2)
+    _   -> error "Calicurating error"
     where
       n1 = fst $ pop s
       s1 = snd $ pop s
