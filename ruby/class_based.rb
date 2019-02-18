d5cf944d5b05bbd31e1575cc4eac997aaeaf9fd5
mkanenobu <kaneda.1018@gmail.com>
2019-02-18 16:02:05 +0900

    02/18/19 04:02:05 PM

--- ruby/class_based.rb
+++ ruby/class_based.rb
@@ -9,3 +9,16 @@ end
 
 # Class detect
 puts a.class()
+
+class SampleClass
+  def setVal1(val1)
+    @@val1 = val1
+  end
+
+  def putVal1()
+    puts @@val1
+  end
+end
+
+SampleClass.new.setVal1(13)
+SampleClass.new.putVal1()
--- ruby/dc.rb
+++ ruby/dc.rb
@@ -5,36 +5,40 @@
 stack = []
 operaters = ['+', '-', '*', '/', '.s', '.']
 
+def calc(stack, op)
+  case op
+    when '+' then
+      tmp = stack.pop
+      tmp = tmp + stack.pop
+      stack.push(tmp)
+    when '-' then
+      tmp = stack.pop
+      tmp = tmp - stack.pop
+      stack.push(tmp)
+    when '*' then
+      tmp = stack.pop
+      tmp = tmp * stack.pop
+      stack.push(tmp)
+    when '/' then
+      tmp = stack.pop
+      tmp = stack.pop / tmp
+      stack.push(tmp)
+    when '.s' then
+      p stack
+    when '.' then
+      p stack.pop()
+  end
+end
+
 while true do
   print "> " # prompt
   line = STDIN.gets
   line = line.split(' ')
   line.each do |w|
     if operaters.include?(w) then
-      case w
-      when '+' then
-        tmp = stack.pop
-        tmp = tmp + stack.pop
-        stack.push(tmp)
-      when '-' then
-        tmp = stack.pop
-        tmp = tmp - stack.pop
-        stack.push(tmp)
-      when '*' then
-        tmp = stack.pop
-        tmp = tmp * stack.pop
-        stack.push(tmp)
-      when '/' then
-        tmp = stack.pop
-        tmp = stack.pop / tmp
-        stack.push(tmp)
-      when '.s' then
-        p stack
-      when '.' then
-        p stack.pop()
-      end
+      calc(stack, w)
     else
-      stack.push(w.to_i)
+      stack.push(w.to_f)
     end
   end
 end
