35a6347ebe5c3356d630f4c021d68a132216c3ec
Mitsuru Kanenobu <kaneda.1018@gmail.com>
2019-04-03 17:06:57 +0900

    add tarai

---
+++ javascript/tarai.js
@@ -0,0 +1,10 @@
+
+const tarai = (x, y, z) => {
+  if (x <= y) {
+    return y
+  } else {
+    return tarai(tarai(x - 1, y, z), tarai(y - 1, z, x), tarai(z - 1, x, y))
+  }
+};
+
+console.log(tarai(12, 6, 0));
---
+++ python/tarai.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+def tarai(x, y, z):
+    if x <= y:
+        return y
+    else:
+        return tarai(tarai(x - 1, y, z), tarai(y - 1, z, x), tarai(z - 1, x, y))
+
+print(tarai(12, 6, 0))
