27848d81ec4b5a8ccaa6a8ecb42fa40c9d415187
mkanenobu <kaneda.1018@gmail.com>
2019-04-13 16:25:12 +0900

    04/13/19 16:25

--- nim/fibonacci.nim
+++ nim/fibonacci.nim
@@ -1,13 +1,13 @@
 import sequtils,strutils,os
 proc main(n:int):int =
-    var
-        a:int = 0
-        b:int = 1
-    if n == 0: return 0
-    else:
-        for i in 1..<n:
-            (a, b) = (b, a + b)
-        return b
+  var
+    a:int = 0
+    b:int = 1
+  if n == 0: return 0
+  else:
+    for i in 1..<n:
+      (a, b) = (b, a + b)
+    return b
 
 # echo main(commandLineParams()[0].parseInt)
 for j in 0..1000:
---
+++ ocaml/fibonacci.ml
@@ -0,0 +1,20 @@
+let rec fibo n =
+  if n = 0 then 0
+  else if n = 1 then 1
+  else (fibo (n - 1)) + (fibo (n - 2))
+;;
+
+let fibo_tail n =
+  let rec iter i n1 n2 =
+    if i <= 0 then n2
+    else if i = 1 then n2
+    else iter (i - 1) n2 (n1 + n2)
+  in
+  iter n 0 1
+;;
+
+let () =
+  (* Printf.printf "%d\n" (fibo_tail 10) *)
+  for i = 0 to 60 do
+    Printf.printf "%d\n" (fibo_tail i);
+  done
