fa5e3bd171686809dbf21ce47857425d8a2e62a4
mkanenobu <kaneda.1018@gmail.com>
2019-04-19 20:41:35 +0900

    04/19/19 20:41

--- ocaml/fibonacci.ml
+++ ocaml/fibonacci.ml
@@ -4,17 +4,28 @@ let rec fibo n =
   else (fibo (n - 1)) + (fibo (n - 2))
 ;;
 
+let rec fibo_pair n =
+  if n = 1 then (1, 0)
+  else
+    let (i, j) = fibo_pair (n - 1) in
+    (i + j, i)
+;;
+
+let fibo_pair_ n =
+  let (n, _) = fibo_pair n in
+  n
+;;
+
 let fibo_tail n =
   let rec iter i n1 n2 =
-    if i <= 0 then n2
-    else if i = 1 then n2
+    if i = 1 then n2
     else iter (i - 1) n2 (n1 + n2)
   in
   iter n 0 1
 ;;
 
 let () =
-  for i = 0 to 100 do
+  for i = 1 to 100 do
     Printf.printf "% d\n" (match fibo_tail i with
         | n when n < 0 -> Printf.printf "overflow"; exit 1;
         | n -> n);
