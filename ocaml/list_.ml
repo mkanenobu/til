b00cf2d971989ba1d11a5395299324bcbcccad28
mkanenobu <kaneda.1018@gmail.com>
2019-04-20 20:27:35 +0900

    04/20/19 20:27

--- ocaml/list_.ml
+++ ocaml/list_.ml
@@ -4,6 +4,9 @@ let l1 = List.init 5 (fun n -> n);;
 (* initialize [5; 6; 7] *)
 let l2 = List.init 3 (fun n -> n + 5);;
 
+(* 1からnまでの奇数 *)
+let odd_list n = List.filter (fun n_ -> n_ mod 2 <> 0) (List.init n (fun n' -> n' + 1));;
+
 let () =
   List.iter (fun e -> printf "%d " e) l1; print_string "\n";
   List.iter (fun e -> printf "%d " e) l2; print_string "\n";
@@ -14,3 +17,4 @@ let () =
   (* get nth element *)
   printf "%d\n" (List.nth l1 3);
 
+  List.iter (fun e -> printf "%d " e) (odd_list 11); print_string "\n";
