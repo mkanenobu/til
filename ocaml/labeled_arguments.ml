17415355625bc4e0c177dfdeeb4cc2f55ffd446a
mkanenobu <kaneda.1018@gmail.com>
2019-04-20 21:15:34 +0900

    04/20/19 21:15

---
+++ ocaml/labeled_arguments.ml
@@ -0,0 +1,14 @@
+let f1 ~first:a ~second:b =
+  a - b
+;;
+
+let f2 ~first ~second =
+  first - second
+;;
+
+let () =
+  Printf.printf "%d\n" (f1 ~first:10 ~second:1);
+  (* same *)
+  Printf.printf "%d\n" (f1 ~second:1 ~first:10);
+
+  Printf.printf "%d\n" (f2 ~second:1 ~first:10);
---
+++ ocaml/optional_arguments.ml
@@ -0,0 +1,15 @@
+(* like List.init (max 100) *)
+let gen ?(start = 0) end_ =
+  let rec iter n (list_: int list) =
+    if n > end_ then List.rev list_
+    else if start >= end_ then []
+    else iter (n + 1) (n :: list_)
+  in
+  iter start []
+;;
+
+let () =
+  List.iter (fun e -> Printf.printf "%d " e) (gen ~start:3 10);
+  print_string "\n\n";
+  List.iter (fun e -> Printf.printf "%d " e) (gen 10);
+
