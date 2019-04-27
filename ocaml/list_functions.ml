9a527c0e71844939b8960a443bf290fe03847028
mkanenobu <kaneda.1018@gmail.com>
2019-04-27 20:13:43 +0900

    04/27/19 20:13

--- ocaml/list_functions.ml
+++ ocaml/list_functions.ml
@@ -1,5 +1,18 @@
 open Printf;;
 
+let print_list l =
+  let len = List.length l in
+  print_string "[";
+  List.iteri (
+    fun i e ->
+      printf "%d;" e;
+      if i = len - 1 then
+        printf "]"
+      else
+        printf " "
+  ) l
+;;
+
 let l = [5; 4; 3; 2; 1];;
 
 let () =
@@ -11,4 +24,22 @@ let () =
   List.iteri (
     fun i e ->
       printf "index: %d, element: %d\n" i e;
-  ) l
+  ) l;
+
+  (* map *)
+  let triple = List.map (fun n -> n * 3) l in
+  print_list triple;
+
+  print_endline "";
+
+  (* Handred times other than 2nd element (map with index) *)
+  let hundred = List.mapi (
+      fun i n ->
+        match i with
+        | 1 -> n
+        | _ -> n * 100
+    ) l
+  in
+
+  print_list hundred;
+
