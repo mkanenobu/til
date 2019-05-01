d33aa7b904c5e7e4f5f307643c9425130273f303
mkanenobu <kaneda.1018@gmail.com>
2019-05-01 22:33:57 +0900

    05/01/19 22:33

--- ocaml/type_declare.ml
+++ ocaml/type_declare.ml
@@ -1,14 +1,28 @@
 (* other name *)
 type integer = int
-type pair_of_ints = None | Some of pair_of_ints
+type pair_of_ints = int * int
+(* optional type *)
+type pair_of_ints_op = None | Some of pair_of_ints
 type my_tuple = string * string
+
+(* variant type *)
+(* それぞれがコンストラクタ、これらによって初期化された値が型`foo`を持つ *)
+type foo =
+  | Nothing
+  | Int of int
+  | Pair of int * int
+  | String of string
 
 let () =
   let n: integer = 10 in
-  (* same instance *)
+  let _pair: pair_of_ints_op = Some (3, 5) in
+  (* _string typed `foo` *)
+  let _string = String "string foo" in
+
+  (* inherit super class *)
   Printf.printf "%d\n" n;
 
-  let (tup_one, tup_two) = ("string1", "string2") in
+  let (tup_one, tup_two):my_tuple = ("string1", "string2") in
   Printf.printf "%s\n" tup_one;
   Printf.printf "%s\n" tup_two;
 
