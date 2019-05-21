6d0cbeba88230abb143eb73a20162ce384f5d92e
mkanenobu <kaneda.1018@gmail.com>
2019-05-22 08:11:14 +0900

    05/22/19 08:11

---
+++ ocaml/mylib/mylib__List.ml
@@ -0,0 +1,53 @@
+open Printf
+
+let print_int_list l =
+  let len = List.length l in
+  print_string "[";
+  List.iteri (
+    fun i e ->
+      printf "%d" e;
+      if i = len - 1 then
+        printf "]"
+      else
+        printf " "
+  ) l
+;;
+
+let print_string_list l =
+  let len = List.length l in
+  print_string "[";
+  List.iteri (
+    fun i e ->
+      printf "%S" e;
+      if i = len - 1 then
+        printf "]"
+      else
+        printf " "
+  ) l
+;;
+
+let print_float_list l =
+  let len = List.length l in
+  print_string "[";
+  List.iteri (
+    fun i e ->
+      printf "%F" e;
+      if i = len - 1 then
+        printf "]"
+      else
+        printf " "
+  ) l
+;;
+
+let print_float_list l =
+  let len = List.length l in
+  print_string "[";
+  List.iteri (
+    fun i e ->
+      printf "%F" e;
+      if i = len - 1 then
+        printf "]"
+      else
+        printf " "
+  ) l
+;;
