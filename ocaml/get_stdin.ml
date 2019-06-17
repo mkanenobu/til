07eab000c29a08141ee5ad6301c369e4ca019729
mkanenobu <kaneda.1018@gmail.com>
2019-06-17 22:42:58 +0900

    2019年  6月 17日 月曜日 22:42:58 JST

---
+++ ocaml/get_stdin.ml
@@ -0,0 +1,18 @@
+open Printf
+
+let rec read_lines ic =
+  try
+    let line = input_line ic in
+    line :: read_lines ic
+  with End_of_file -> []
+;;
+
+let split_line line =
+  Str.split (Str.regexp "[ \t]+") line
+;;
+
+let () =
+  let lines = read_lines stdin in
+  List.iter (fun e -> printf "%s;;" e) lines
+;;
+
