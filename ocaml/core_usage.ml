62fb5dde1b1221e70f73af90188d9a83388181c0
mkanenobu <kaneda.1018@gmail.com>
2019-07-14 14:06:41 +0900

    Sun Jul 14 14:06:41 JST 2019

--- ocaml/core_usage.ml
+++ ocaml/core_usage.ml
@@ -1,12 +1,12 @@
 open Core
 
 let () =
-  let list_count_down = List.init 10 ~f:(fun n -> abs (n - 10)) in
+  let list_count_down n = List.init n ~f:(fun n' -> abs (n' - n)) in
   let l = List.init 10 (fun n -> n) in
   (* printfがCore直下に入っている *)
   (* CoreではLabeled Argumentが多用されている *)
   List.iter l ~f:(fun n -> printf "%d\n" n);
-  List.iteri list_count_down ~f:(fun i n -> printf "%d番目: %d\n" i n);
+  List.iteri (list_count_down 12) ~f:(fun i n -> printf "%d番目: %d\n" i n);
 
   let str = String.strip "    xyz    " in
   printf "%S\n" str;
