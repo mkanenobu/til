684292e753a70f65265927cf7b388d15815e6051
mkanenobu <mitsuru.kanenobu@gmail.com>
2024-04-07 00:29:05 +0900

    Sun Apr  7 00:29:05 JST 2024

--- others/zig/reverse-polish-calculator/src/main.zig
+++ others/zig/reverse-polish-calculator/src/main.zig
@@ -10,7 +10,11 @@ pub fn main() !void {
     var gpa = std.heap.GeneralPurposeAllocator(.{}){};
     var allocator = gpa.allocator();
     var stack = Stack.init(allocator);
-    defer stack.deinit();
+    defer {
+        stack.deinit();
+        const gpaDeinitStatus = gpa.deinit();
+        std.debug.assert(gpaDeinitStatus == .ok);
+    }
 
     var stdoutWriter = std.io.getStdOut().writer();
     var stdinReader = std.io.getStdIn().reader();
@@ -18,14 +22,18 @@ pub fn main() !void {
     var tokenBuffer = ArrayList(token.Token).init(allocator);
     defer tokenBuffer.deinit();
 
-    startRepl(stdinReader, stdoutWriter, &stack) catch unreachable;
+    try startRepl(stdinReader, stdoutWriter, &stack);
 }
 
 fn startRepl(reader: anytype, writer: anytype, stack: *Stack) !void {
     var gpa = std.heap.GeneralPurposeAllocator(.{}){};
     var allocator = gpa.allocator();
     var tokenBuffer = ArrayList(token.Token).init(allocator);
-    defer tokenBuffer.deinit();
+    defer {
+        tokenBuffer.deinit();
+        const gpaDeinitStatus = gpa.deinit();
+        std.debug.assert(gpaDeinitStatus == .ok);
+    }
 
     var inputBuffer: [1024 * 1024]u8 = undefined;
 
@@ -44,6 +52,20 @@ fn startRepl(reader: anytype, writer: anytype, stack: *Stack) !void {
         }
         utils.print("{any}\n", .{stack.stack.items});
     }
+}
+
+test "startRepl" {
+    var stack = Stack.init(std.testing.allocator);
+    defer stack.deinit();
+
+    // var tmpDirIterable = std.testing.tmpIterableDir(.{}).iterable_dir;
+    // // Use tmp file instead of stdin for testing
+    // var file = try tmpDirIterable.dir.createFile("test.txt", .{ .read = true });
+    // var reader = file.reader();
+    // var arr = std.ArrayList(u8).init(std.testing.allocator);
+    // var writer = arr.writer();
+
+    // try startRepl(reader, writer, &stack);
 }
 
 comptime {
