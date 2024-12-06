2da9cc6b99461643a2b8d279d32f47679be7e818
mkanenobu <mitsuru.kanenobu@gmail.com>
2024-12-06 20:06:52 +0900

    Fri Dec  6 20:06:52 JST 2024

--- scala/tour-of-scala/multiple-parameter-lists/multiple_parameter_lists.scala
+++ scala/tour-of-scala/multiple-parameter-lists/multiple_parameter_lists.scala
@@ -12,4 +12,40 @@ def main(): Unit = {
     val res = numbers.foldLeft(0)((m, n) => m + n)
     println(res)
   }
+
+  {
+    // multiple parameter listsのユースケース
+    def foldLeft1[A, B](as: List[A], b0: B, op: (B, A) => B): B =
+      as.foldLeft(b0)(op)
+    val numbers = List(1, 2, 3, 4, 5)
+    // foldLeft1メソッドをこのように呼び出したいがコンパイルが通らない（NOTE: scala runだと何故か通る）
+    // val res = foldLeft1(numbers, 0, _ + _)
+
+    // これは関数opの型を推論出来ないため
+    // Scalaはパラメータの型推論をパラメータリスト単位で行うため
+    // opの型に利用されているA, Bはasとb0によって解決されるのでas, b0とopが同じパラメータリストにあると型が解決出来ない
+    // これを解決するためには下記のいずれかの方法で呼び出す必要がある
+    // 1. ジェネリクスの型を明示的に指定する
+    val first = foldLeft1[Int, Int](numbers, 0, _ + _)
+    // 2. 関数opの型を明示する
+    val second = foldLeft1(numbers, 0, (a: Int, b: Int) => a + b)
+
+    // オリジナルのfoldLeftのようにパラメータリストごと分ければそもそもそういった問題が発生しない
+    def foldLeft2[A, B](as: List[A], b0: B)(op: (B, A) => B) =
+      as.foldLeft(b0)(op)
+    val third = foldLeft2(numbers, 0)(_ + _)
+  }
+
+  {
+    // 部分適用
+    // メソッドが少ないパラメータリストで呼び出されると不足しているパラメータリストを引数として取る関数が生成される
+    // これは部分適用と呼ばれる
+    val numbers = List(1, 2, 3, 4, 5)
+    val numberFunc = numbers.foldLeft(List[Int]())
+
+    val squares = numberFunc((xs, x) => xs :+ x*x)
+    println(squares)
+    val cubes = numberFunc((xs, x) => xs :+ x*x*x)
+    println(cubes)
+  }
 }
