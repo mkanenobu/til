6d862cfe3109751d3e9fd159a117f55ee667896f
mkanenobu <mitsuru.kanenobu@gmail.com>
2025-01-07 19:42:04 +0900

    Tue Jan  7 19:42:04 JST 2025

--- others/zig/repl/src/repl.zig
+++ others/zig/repl/src/repl.zig
@@ -5,14 +5,23 @@ fn readLine(reader: fs.File.Reader, buffer: []u8) !?[]u8 {
     return try reader.readUntilDelimiterOrEof(buffer, '\n');
 }
 
-pub fn repl(reader: fs.File.Reader, writer: fs.File.Writer) !void {
+pub fn repl(reader: fs.File.Reader, rawWriter: fs.File.Writer) !void {
+    var writer = std.io.bufferedWriter(rawWriter);
     var buffer: [1024]u8 = undefined;
 
     while (true) {
-        try writer.writeAll("> ");
-        const input = (try readLine(reader, &buffer)).?;
-        try writer.writeAll("you entered: ");
-        try writer.writeAll(input);
-        try writer.writeAll("\n");
+        _ = try writer.write("> ");
+        try writer.flush();
+
+        const input = try readLine(reader, &buffer);
+        if (input == null) {
+            // EOF
+            break;
+        }
+        const i = input.?;
+        _ = try writer.write("you entered: ");
+        _ = try writer.write(i);
+        _ = try writer.write("\n");
+        try writer.flush();
     }
 }
