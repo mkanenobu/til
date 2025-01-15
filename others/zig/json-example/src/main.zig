1312ecee77c502e244d2b383ed124ad346af39d3
mkanenobu <mitsuru.kanenobu@gmail.com>
2025-01-15 15:57:07 +0900

    Wed Jan 15 15:57:07 JST 2025

--- others/zig/json-example/src/main.zig
+++ others/zig/json-example/src/main.zig
@@ -2,42 +2,42 @@ const std = @import("std");
 const json = std.json;
 const print = std.debug.print;
 
-const Schema = struct {
+const JsonSchema = struct {
     key: []const u8,
     num: f64,
     nested: Nested,
-    array_field: []const []const u8,
+    array_field: []u8,
 
     const Nested = struct {
         nested_value: []const u8,
     };
-
-    
 };
 
 pub fn main() !void {
     const json_file = @embedFile("./test.json");
 
+    const allocator = std.heap.page_allocator;
+
     // validate
-    print("json file is valid: {}\n", .{try json.validate(std.heap.page_allocator, json_file)});
+    print("json file is valid: {}\n", .{try json.validate(allocator, json_file)});
 
     // parse
-    const parsed = try json.parseFromSlice(Schema, std.heap.page_allocator, json_file, .{});
+    const parsed = try json.parseFromSlice(JsonSchema, allocator, json_file, .{});
     defer parsed.deinit();
 
     print("value: {any}\n", .{parsed.value});
-    print("{s}\n", .{parsed.value.key});
+    print("key: {s}\n", .{parsed.value.key});
 
     // stringify
-    var buf = std.ArrayList(u8).init(std.heap.page_allocator);
+    var buf = std.ArrayList(u8).init(allocator);
     defer buf.deinit();
 
-    try json.stringify(struct {
-        key: []const u8,
-        number: u32,
-    }{
+    try json.stringify(.{
         .key = "こんにちは",
         .number = 3,
+        // u8の配列・スライスだと文字列としてエンコードされる
+        .array = [_]u4{ 1, 2, 3, 4, 5 },
+        .array_of_string = [_][]const u8{ "こんにちは", "これは配列のフィールドです" },
     }, .{ .whitespace = .indent_2 }, buf.writer());
     print("stringified: {s}\n", .{buf.items});
 }
