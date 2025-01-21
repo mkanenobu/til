e6244196037d5bf8b6a7858feefff2bd2e0060ea
mkanenobu <mitsuru.kanenobu@gmail.com>
2025-01-21 18:35:21 +0900

    Tue Jan 21 18:35:21 JST 2025

--- go/cache-with-expiration/main.go
+++ go/cache-with-expiration/main.go
@@ -3,26 +3,39 @@ package main
 import (
 	"fmt"
 	"github.com/mkanenobu/til/cache-with-expiration/cache"
+	"runtime"
+	_ "runtime/pprof"
 	"time"
 )
 
 func main() {
-	c := cache.New[int](5 * time.Second)
+	if err := runtime.StartTrace(); err != nil {
+		panic(err)
+	}
+
+	c := cache.New[int](1 * time.Second)
 	c.Set("one", 1, 1*time.Second)
-	c.Set("two", 2, 1*time.Second)
+	c.Set("two", 2, 5*time.Second)
 
 	v, ok := c.Get("one")
 	if ok {
-		fmt.Printf("%+v\n", *v)
+		fmt.Printf("one: %d\n", *v)
 	} else {
-		fmt.Println("Not found")
+		fmt.Println("one: Not found")
 	}
 
-	time.Sleep(2 * time.Second)
+	time.Sleep(3 * time.Second)
 	v, ok = c.Get("one")
 	if ok {
-		fmt.Printf("%+v\n", *v)
+		fmt.Printf("one: %d\n", *v)
 	} else {
-		fmt.Println("Not found")
+		fmt.Println("one: Not found")
+	}
+
+	v, ok = c.Get("two")
+	if ok {
+		fmt.Printf("two: %d\n", *v)
+	} else {
+		fmt.Println("two: Not found")
 	}
 }
