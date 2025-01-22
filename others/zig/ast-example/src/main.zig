5bdab379e07c449d16fd71aeb8b41385116dd072
mkanenobu <mitsuru.kanenobu@gmail.com>
2025-01-22 20:01:38 +0900

    Wed Jan 22 20:01:38 JST 2025

---
+++ others/zig/ast-example/src/main.zig
@@ -0,0 +1,29 @@
+const std = @import("std");
+const Ast = std.zig.Ast;
+const print = std.debug.print;
+
+pub fn main() !void {
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    defer if (gpa.deinit() == .leak) {
+        std.debug.panic("leaks detected", .{});
+    };
+    const allocator = gpa.allocator();
+
+    const source = @embedFile("./main.zig");
+    var ast = try Ast.parse(allocator, source[0..], .zig);
+    defer ast.deinit(allocator);
+
+    const firstNode = ast.nodes.get(0);
+    print("firstNode tag: {any}\n", .{firstNode.tag});
+    const lastNode = ast.nodes.get(ast.nodes.len - 1);
+    print("lastNode tag: {any}\n", .{lastNode.tag});
+
+    const firstToken = ast.tokens.get(0);
+    print("firstToken tag: {any}\n", .{firstToken.tag});
+    const lastNodesMainToken = ast.tokens.get(lastNode.main_token);
+    print("lastNode'sMainToken tag: {any}\n", .{lastNodesMainToken.tag});
+}
+
+test {
+    std.testing.refAllDecls(@This());
+}
